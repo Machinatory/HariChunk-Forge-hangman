ec91ca7ef4ff1ef7aaa5673e334f9c5024aad4c9
JustHari01 <JustHari01@users.noreply.github.com>
2026-04-28 18:09:31 +0700

    Fix MixinChunk: correct SRG names for ChunkAccess fields (f_187603_, f_187604_)

--- harichunk_opts_scheduling/src/main/java/com/hari/harichunk/opts/scheduling/mixin/idle_tasks/autosave/enhanced_autosave/MixinChunk.java
+++ harichunk_opts_scheduling/src/main/java/com/hari/harichunk/opts/scheduling/mixin/idle_tasks/autosave/enhanced_autosave/MixinChunk.java
@@ -15,16 +15,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ChunkAccess.class)
 public abstract class MixinChunk {
 
-    @Shadow(remap = false) protected volatile boolean f_62662_; // unsaved
+    @Shadow(remap = false) protected volatile boolean f_187603_; // unsaved
 
-    @Shadow(remap = false) @Final private ChunkPos f_62661_; // pos
+    @Shadow(remap = false) @Final private ChunkPos f_187604_; // chunkPos
 
     @Inject(method = "*", at = @At(value = "FIELD", target = "Lnet/minecraft/world/level/chunk/ChunkAccess;unsaved:Z", shift = At.Shift.AFTER))
     private void onSetShouldSave(CallbackInfo ci) {
         //noinspection ConstantConditions
-        if (this.f_62662_ && (Object) this instanceof LevelChunk worldChunk) {
+        if (this.f_187603_ && (Object) this instanceof LevelChunk worldChunk) {
             if (worldChunk.getLevel() instanceof ServerLevel serverWorld) {
-                ((IThreadedAnvilChunkStorage) serverWorld.getChunkSource().chunkMap).enqueueDirtyChunkPosForAutoSave(this.f_62661_);
+                ((IThreadedAnvilChunkStorage) serverWorld.getChunkSource().chunkMap).enqueueDirtyChunkPosForAutoSave(this.f_187604_);
             }
         }
     }
